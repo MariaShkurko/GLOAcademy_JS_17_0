65ba0ae9a943bf5403ea06dd2999b0387b92a964
Maria Shkurko <shkurko_mashenka@mail.ru>
2021-01-29 21:58:29 +0300

    Инициализированы переменные, добавлен вывод в консоль

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 100000,
+    income = 'фриланс',
+    addExpenses = 'интернет, такси, коммуналка',
+    deposit = true,
+    mission = 2000000,
+    period = 10,
+    budgetDay = money / 30;
 
-alert('Lesson 1');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
 
-console.log('Lesson 1');
+console.log(addExpenses.length);
+
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log(budgetDay);
