e9ac1336267ff3276167f5d64fad326802667c86
Maria Shkurko <shkurko_mashenka@mail.ru>
2021-03-01 13:47:16 +0300

    lesson 19 is completed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    function countTimer(deadline) {
+    // Timer
+    const countTimer = deadline => {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
 
-        function getTimeRemaining() {
+        const getTimeRemaining = () => {
             const dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
                 timeRemaining = (dateStop - dateNow) / 1000,
@@ -14,9 +15,9 @@ window.addEventListener('DOMContentLoaded', () => {
                 hours = Math.floor(timeRemaining / 3600);
 
             return { timeRemaining, hours, minutes, seconds };
-        }
+        };
 
-        function updateClock() {
+        const updateClock = () => {
             const timer = getTimeRemaining();
 
             const formatNumbers = number => {
@@ -30,7 +31,6 @@ window.addEventListener('DOMContentLoaded', () => {
             timerHours.textContent = formatNumbers(timer.hours);
             timerMinutes.textContent = formatNumbers(timer.minutes);
             timerSeconds.textContent = formatNumbers(timer.seconds);
-            console.log(timer.timeRemaining);
 
             if (timer.timeRemaining < 1) {
                 timerHours.textContent = '00';
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 timerSeconds.textContent = '00';
                 return 0;
             }
-        }
+        };
 
         updateClock();
         const idInterval = setInterval(() => {
@@ -47,7 +47,59 @@ window.addEventListener('DOMContentLoaded', () => {
                 clearInterval(idInterval);
             }
         }, 1000);
-    }
+    };
 
     countTimer('2 march 2021');
+
+    // Menu
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = menu.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        let count = -100,
+            animationInterval;
+
+        // Animation menu
+        const animationMenu = () => {
+            animationInterval = requestAnimationFrame(animationMenu);
+            count += 5;
+            menu.style.transform = `translateX(${count}%)`;
+            if (menu.style.transform === 'translateX(100%)') cancelAnimationFrame(animationInterval);
+        };
+
+        const handlerMenu = () => {
+            if (screen.width < 768) {
+                menu.classList.toggle('active-menu');
+            } else {
+                if (!menu.style.transform || menu.style.transform === 'translateX(-100%)') {
+                    console.log('start animation');
+                    count = -100;
+                    animationMenu();
+                } else {
+                    console.log('start close');
+                    menu.style.transform = '';
+                }
+            }
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach(item => item.addEventListener('click', handlerMenu));
+    };
+
+    toggleMenu();
+
+    // popup
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupClose = popup.querySelector('.popup-close'),
+            popupBtn = document.querySelectorAll('.popup-btn');
+
+        popupBtn.forEach(item => item.addEventListener('click', () => popup.style.display = 'block'));
+        popupClose.addEventListener('click', () => popup.style.display = '');
+    };
+
+    togglePopUp();
 });
